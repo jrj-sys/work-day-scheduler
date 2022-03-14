6eea637016e363add65c4b1acbbac057d585750c
jrj-sys <jamesjamel99@gmail.com>
2022-03-13 23:52:13 -0500

    add dynamic class changing, add textarea input, add localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,47 @@
 // The current day is displayed at the top of the calendar
-var today = function () {
-    var now = moment().format("dddd, MMMM Do");
-    $( "#currentDay" ).html(now);
+var now = moment().format("dddd, MMMM Do");
+$( "#currentDay" ).html(now);
 
-}
-
-
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+$( ".save-btn" ).on("click", function() {
+    // retrieve textarea user input & the current time 
+    var userText = $( ".description" ).val();
+    var currentHour = $( ".description" ).attr("id");
     
-    
-    
-    
+    // send to localStorage 
+    localStorage.setItem(currentHour, userText);
 
-
-
-
-
-
+});
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
+var timeColor = function () {
+    // get the current time of day in military time
+    var now = moment().hour();
+    // loop over the time slots and add/remove color classes to update
+    $( ".description" ).each(function () {
+        var blockCurrentTime = parseInt($(this).attr("id").split("h")[0]);
+        
+        if (blockCurrentTime < now) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (blockCurrentTime === now) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } 
+    })
+}
+
+
 // WHEN I refresh the page
 // THEN the saved events persist
+$(".description #09h").text(localStorage.getItem("09h"));
 
-today();
+timeColor();
